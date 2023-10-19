67185a31223e77aa7693854bc624246a11f29197
nortif8 <nortif8@gmail.com>
2023-10-19 14:50:47 -0600

    Added initial skeleton code for android project

---
+++ app/src/main/java/com/example/ezvault/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ezvault;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
