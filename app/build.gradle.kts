f891579ddd425cabbea0b9ec2c34032626326433
nortif8 <144505555+nortif8@users.noreply.github.com>
2023-10-24 17:03:23 -0600

    Revert "add barcode-scanning dep (mlkit)"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
