345a62d0b1178c9eb4fd4ef72837b4b3e52236c7
nortif8 <nortif8@gmail.com>
2023-10-29 14:06:36 -0600

    Add FirebaseBundle

--- app/src/main/java/com/example/ezvault/MainActivity.java
+++ app/src/main/java/com/example/ezvault/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.ezvault;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseBundle firebase = new FirebaseBundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
