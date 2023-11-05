2a54f100d7113fd9f556f9b81b7baee6f980c32e
nortif8 <nortif8@gmail.com>
2023-11-05 13:58:08 -0700

    Implement create

--- app/src/main/java/com/example/ezvault/database/RawUserDAO.java
+++ app/src/main/java/com/example/ezvault/database/RawUserDAO.java
@@ -2,17 +2,17 @@ package com.example.ezvault.database;
 
 import com.example.ezvault.FirebaseBundle;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Transaction;
 
 import java.util.ArrayList;
 
-import javax.annotation.Nullable;
-
 public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
     private final String collectionName = "users";
     private final FirebaseFirestore db;
+
     /**
      * Constructs a RawUserDAO
      *
@@ -25,26 +25,28 @@ public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
 
     @Override
     Task<String> create(RawUser rawUser) {
-        return null;
+        Task<DocumentReference> t = db.collection(collectionName)
+                .add(rawUser);
+        return t.continueWith(task -> task.getResult().getId());
     }
 
     @Override
     public Task<RawUser> read(String s) {
-         Task<DocumentSnapshot> t = db.collection(collectionName)
+        Task<DocumentSnapshot> t = db.collection(collectionName)
                 .document(s)
                 .get();
-         return t.continueWith(task -> {
-             if (task.isSuccessful()) {
-                 DocumentSnapshot d = task.getResult();
-                 String userName = d.getString("name");
-                 ArrayList<String> tags = (ArrayList<String>) d.get("tagids");
-                 ArrayList<String> items = (ArrayList<String>) d.get("itemids");
+        return t.continueWith(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot d = task.getResult();
+                String userName = d.getString("name");
+                ArrayList<String> tags = (ArrayList<String>) d.get("tagids");
+                ArrayList<String> items = (ArrayList<String>) d.get("itemids");
 
-                 return new RawUser(userName, tags, items);
-             } else {
-                 throw new UnsupportedOperationException("unimplemeted");
-             }
-         });
+                return new RawUser(userName, tags, items);
+            } else {
+                throw new UnsupportedOperationException("unimplemeted");
+            }
+        });
     }
 
     @Override
