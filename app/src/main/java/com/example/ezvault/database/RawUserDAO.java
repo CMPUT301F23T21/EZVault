90849241eb1b68bf5db5ab458c62a8c10e9f1375
nortif8 <nortif8@gmail.com>
2023-11-05 13:51:54 -0700

    Implement read for raw user, implement utility methods for raw user

--- app/src/main/java/com/example/ezvault/database/RawUserDAO.java
+++ app/src/main/java/com/example/ezvault/database/RawUserDAO.java
@@ -2,9 +2,17 @@ package com.example.ezvault.database;
 
 import com.example.ezvault.FirebaseBundle;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Transaction;
 
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
+
 public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
+    private final String collectionName = "users";
+    private final FirebaseFirestore db;
     /**
      * Constructs a RawUserDAO
      *
@@ -12,6 +20,7 @@ public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
      */
     public RawUserDAO(FirebaseBundle firebase) {
         super(firebase);
+        db = firebase.getDb();
     }
 
     @Override
@@ -20,8 +29,22 @@ public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
     }
 
     @Override
-    Task<RawUser> read(String s) {
-        return null;
+    public Task<RawUser> read(String s) {
+         Task<DocumentSnapshot> t = db.collection(collectionName)
+                .document(s)
+                .get();
+         return t.continueWith(task -> {
+             if (task.isSuccessful()) {
+                 DocumentSnapshot d = task.getResult();
+                 String userName = d.getString("name");
+                 ArrayList<String> tags = (ArrayList<String>) d.get("tagids");
+                 ArrayList<String> items = (ArrayList<String>) d.get("itemids");
+
+                 return new RawUser(userName, tags, items);
+             } else {
+                 throw new UnsupportedOperationException("unimplemeted");
+             }
+         });
     }
 
     @Override
@@ -55,6 +78,35 @@ public class RawUserDAO extends AbstractDAO<RawUserDAO.RawUser, String> {
     }
 
     public static class RawUser {
+        protected String userName;
+        protected ArrayList<String> tags;
+        protected ArrayList<String> items;
 
+        @Override
+        public String toString() {
+            return "RawUser{" +
+                    "userName='" + userName + '\'' +
+                    ", tags=" + tags +
+                    ", items=" + items +
+                    '}';
+        }
+
+        public RawUser(String userName, ArrayList<String> tags, ArrayList<String> items) {
+            this.userName = userName;
+            this.tags = tags;
+            this.items = items;
+        }
+
+        public String getUserName() {
+            return this.userName;
+        }
+
+        public ArrayList<String> getTags() {
+            return tags;
+        }
+
+        public ArrayList<String> getItems() {
+            return items;
+        }
     }
 }
