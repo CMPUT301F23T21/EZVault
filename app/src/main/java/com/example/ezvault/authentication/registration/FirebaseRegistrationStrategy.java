1be0c3010055e8376d5fe10f50ff66a2be17a5b4
nortif8 <nortif8@gmail.com>
2023-11-06 22:00:39 -0700

    RegistrationException added

--- app/src/main/java/com/example/ezvault/authentication/registration/FirebaseRegistrationStrategy.java
+++ app/src/main/java/com/example/ezvault/authentication/registration/FirebaseRegistrationStrategy.java
@@ -6,15 +6,33 @@ import com.example.ezvault.database.UserService;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 
+/**
+ * Represents a registration strategy that involves accessing firebase
+ */
 public abstract class FirebaseRegistrationStrategy implements IRegistrationStrategy {
     private final FirebaseBundle firebase;
 
+    /**
+     * Construct a FirebaseRegistrationStrategy
+     * @param firebase The firebase bundle
+     */
     public FirebaseRegistrationStrategy(FirebaseBundle firebase) {
         this.firebase = firebase;
     }
 
+    /**
+     * Register the use in the firebase auth system
+     * @param firebase The firebase bundle
+     * @param userName The userName of the user
+     * @return Task representing the user's registration in firebase auth
+     */
     public abstract Task<AuthResult> register(FirebaseBundle firebase, String userName);
 
+    /**
+     * Register the user in the firebase auth, and then readUser details
+     * @param userName The user name of the registering user
+     * @return The asynchronous task containing user
+     */
     @Override
     public Task<User> register(String userName) {
         UserService userService = new UserService(this.firebase);
