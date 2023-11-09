77b16d722f6b91e92dd31e2f17aeadb1e189d442
nortif8 <nortif8@gmail.com>
2023-11-09 16:21:50 -0700

    add hilt

---
+++ app/src/main/java/com/example/ezvault/EZVaultApplication.java
@@ -0,0 +1,8 @@
+package com.example.ezvault;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class EZVaultApplication extends Application { }
