ae2542d85e52a50d04ec1be75c8dd2b4c3bdaaf1
nortif8 <nortif8@gmail.com>
2023-11-09 17:38:37 -0700

    Login

--- app/src/main/java/com/example/ezvault/AuthActivity.java
+++ app/src/main/java/com/example/ezvault/AuthActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/ezvault/LoginFragment.java
+++ app/src/main/java/com/example/ezvault/LoginFragment.java
@@ -1,18 +1,32 @@
 package com.example.ezvault;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 
+import com.example.ezvault.authentication.authentication.AuthenticationHandler;
+import com.example.ezvault.authentication.authentication.EmailPasswordAuthenticationStrategy;
+import com.example.ezvault.database.FirebaseBundle;
+import com.example.ezvault.utils.UserManager;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
 /**
  * A simple {@link Fragment} subclass.
  */
+@AndroidEntryPoint
 public class LoginFragment extends Fragment {
+    @Inject
+    UserManager userManager;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -33,6 +47,29 @@ public class LoginFragment extends Fragment {
 
         backButton.setOnClickListener(v -> {
             getParentFragmentManager().popBackStack();
+        });
+
+        EditText emailText = view.findViewById(R.id.username_text);
+        EditText passwordText = view.findViewById(R.id.password_text);
+        Button loginButton = view.findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(v -> {
+            loginButton.setEnabled(false);
+
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            EmailPasswordAuthenticationStrategy as = new EmailPasswordAuthenticationStrategy(new FirebaseBundle(), email, password);
+            AuthenticationHandler ah = new AuthenticationHandler(as);
+
+            ah.authenticate().addOnSuccessListener(user -> {
+                Log.v("EZVault", "Successful login: " + user.getUid());
+                userManager.setUser(user);
+                getActivity().finish();
+            }).addOnFailureListener(e -> {
+                loginButton.setEnabled(true);
+                Log.e("EZVault", "Failed login.", e);
+            });
         });
 
         return view;
