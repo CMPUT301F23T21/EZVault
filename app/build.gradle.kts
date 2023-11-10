7be7e7f1eae0466074eec834c7c2baa9fa09a1c4
Elwin <eshen1998@gmail.com>
2023-11-09 17:08:50 -0700

    Added dependencies for SafeArgs and Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
     implementation("com.google.mlkit:text-recognition:16.0.0")
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +46,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
 }
