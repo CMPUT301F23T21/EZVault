bdc8f729bfde32d2ba32405860697d6fe51539e5
Elwin <eshen1998@gmail.com>
2023-11-12 04:10:13 -0700

    Can retrieve data from tag dialog in tag fragment, added view item xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+        //classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.5" apply false
+    //id("androidx.navigation.safeargs.kotlin") version "2.7.5" apply false
 }
