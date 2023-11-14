17f6da02a4c05c758be868dad0c9189cb6e4fbac
nortif8 <nortif8@gmail.com>
2023-11-13 19:10:20 -0700

    Add docs to ItemListView.java

--- app/src/main/java/com/example/ezvault/model/utils/ItemListView.java
+++ app/src/main/java/com/example/ezvault/model/utils/ItemListView.java
@@ -2,8 +2,26 @@ package com.example.ezvault.model.utils;
 
 import com.example.ezvault.model.Item;
 
+/**
+ * Represents the functionality needed to view an item list.
+ */
 public interface ItemListView extends Iterable<Item> {
+    /**
+     * Get the number of items that should be viewed.
+     * @return The number of Items that are displayable.
+     */
     int size();
+
+    /**
+     * Check if there are no items that should be displayed.
+     * @return Whether or not there are items to be displayed.
+     */
     boolean isEmpty();
+
+    /**
+     * Get an item.
+     * @param position The position to get the item.
+     * @return The item at that position.
+     */
     Item get(int position);
 }
