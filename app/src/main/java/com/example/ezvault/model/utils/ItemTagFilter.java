462c93327d31ad6cbf88f4da590c8a844ba91055
nortif8 <nortif8@gmail.com>
2023-11-15 20:42:35 -0700

    Add docs and make field private

--- app/src/main/java/com/example/ezvault/model/utils/ItemTagFilter.java
+++ app/src/main/java/com/example/ezvault/model/utils/ItemTagFilter.java
@@ -5,13 +5,23 @@ import com.example.ezvault.model.Tag;
 
 import java.util.Collection;
 
+/**
+ * Item filter based on the inclusion of tags
+ */
 public class ItemTagFilter implements IItemFilter {
-    Collection<Tag> tags;
+    private Collection<Tag> tags;
 
+    /**
+     * Create a new item tag filter
+     * @param tags The tags to test for
+     */
     public ItemTagFilter(Collection<Tag> tags) {
         this.tags = tags;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean keep(Item item) {
         for (Tag tag : tags) {
