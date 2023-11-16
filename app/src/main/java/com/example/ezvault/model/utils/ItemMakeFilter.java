88ec4e571dc6ee2aef9c0b1cb631bb2b329d9be9
nortif8 <nortif8@gmail.com>
2023-11-15 21:23:38 -0700

    Add ItemMakeFilter

---
+++ app/src/main/java/com/example/ezvault/model/utils/ItemMakeFilter.java
@@ -0,0 +1,26 @@
+package com.example.ezvault.model.utils;
+
+import com.example.ezvault.model.Item;
+
+/**
+ * An item filter which ensures items conform to a specific make
+ */
+public class ItemMakeFilter implements IItemFilter {
+    private String make;
+
+    /**
+     * Construct an item filter based on the make
+     * @param make The make to check
+     */
+    public ItemMakeFilter(String make) {
+        this.make = make;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean keep(Item item) {
+        return make.equals(item.getMake());
+    }
+}
