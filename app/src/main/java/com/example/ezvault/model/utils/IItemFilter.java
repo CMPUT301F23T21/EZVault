979f034b78b0cabe923a767999bc4693e146505b
nortif8 <nortif8@gmail.com>
2023-11-15 18:59:17 -0700

    Add interface for an item filter

---
+++ app/src/main/java/com/example/ezvault/model/utils/IItemFilter.java
@@ -0,0 +1,15 @@
+package com.example.ezvault.model.utils;
+
+import com.example.ezvault.model.Item;
+
+/**
+ * An interface for classes that filter items.
+ */
+public interface IItemFilter {
+    /**
+     * Determine whether or not an object passes the filter.
+     * @param item The item to be checked.
+     * @return Whether or not the item should be kept.
+     */
+    boolean keep(Item item);
+}
