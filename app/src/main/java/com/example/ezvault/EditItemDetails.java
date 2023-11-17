e51ac9eb68a500cf9d8a12d9c8367b64da006432
nortif8 <nortif8@gmail.com>
2023-11-17 04:36:10 -0700

    Make EditItemDetails.java work w/o db

--- app/src/main/java/com/example/ezvault/EditItemDetails.java
+++ app/src/main/java/com/example/ezvault/EditItemDetails.java
@@ -2,75 +2,83 @@ package com.example.ezvault;
 
 import android.os.Bundle;
 
-import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.ezvault.model.Item;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EditItemDetails#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class EditItemDetails extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public EditItemDetails() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditItemDetails.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditItemDetails newInstance(String param1, String param2) {
-        EditItemDetails fragment = new EditItemDetails();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        ItemViewModel viewModel = new ViewModelProvider(getActivity()).get(ItemViewModel.class);
-        MutableLiveData<Item> item = viewModel.get();
-        Item raw = item.getValue();
-
-        Log.v("EZVault", "Editing item: " + item.getValue().getId());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_item_details, container, false);
+        MutableLiveData<Item> item = new ViewModelProvider(getActivity()).get(ItemViewModel.class).get();
+        View view = inflater.inflate(R.layout.fragment_edit_item_details, container, false);
+
+        Button saveButton = view.findViewById(R.id.edit_details_save);
+        EditText makeText = view.findViewById(R.id.edit_details_make);
+        EditText modelText = view.findViewById(R.id.edit_details_model);
+        EditText descText = view.findViewById(R.id.edit_details_description);
+        EditText commentText = view.findViewById(R.id.edit_details_comment);
+        EditText countText = view.findViewById(R.id.edit_details_count);
+        EditText valueText = view.findViewById(R.id.edit_details_value);
+        EditText serialText = view.findViewById(R.id.edit_details_serial_number);
+
+        Item raw = item.getValue();
+        Log.v("EZVault", "Editing item: " + raw.getId());
+
+        //set original details
+        makeText.setText(raw.getMake());
+        modelText.setText(raw.getModel());
+        descText.setText(raw.getDescription());
+        commentText.setText(raw.getComment());
+        countText.setText(String.valueOf(raw.getCount()));
+        valueText.setText(String.valueOf(raw.getValue()));
+        serialText.setText(raw.getSerialNumber());
+
+        //update to new values on save
+        saveButton.setOnClickListener(v -> {
+            saveButton.setEnabled(false);
+
+            String make = makeText.getText().toString();
+            String model = modelText.getText().toString();
+            String desc = descText.getText().toString();
+            String comment = commentText.getText().toString();
+            double count = Double.parseDouble(countText.getText().toString());
+            double value = Double.parseDouble(valueText.getText().toString());
+            String serial = serialText.getText().toString();
+
+            raw.setMake(make);
+            raw.setModel(model);
+            raw.setDescription(desc);
+            raw.setComment(comment);
+            raw.setCount(count);
+            raw.setValue(value);
+            raw.setSerialNumber(serial);
+
+            saveButton.setEnabled(true);
+        });
+
+        return view;
     }
 }
