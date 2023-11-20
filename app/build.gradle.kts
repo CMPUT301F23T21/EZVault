86520731dbb006a7163fb46b32e554e3ed196e29
maodus <maodus@ualberta.ca>
2023-11-20 13:02:48 -0700

    Added camerax support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,12 @@ dependencies {
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
     implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.5")
+
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-video:1.3.0")
+
+    implementation ("androidx.camera:camera-view:1.3.0")
+    implementation ("androidx.camera:camera-extensions:1.3.0")
 }
