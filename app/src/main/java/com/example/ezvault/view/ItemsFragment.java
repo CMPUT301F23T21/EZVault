8440c408d86d3881f79c1e8126af4a937e82a0d4
nortif8 <nortif8@gmail.com>
2023-11-25 08:48:35 -0700

    minor(item-fragment): Refactor and fix.
    - Refactors the ItemFragment, eliminating dead code and warnings.
    - Fixes bug where total value has hardcoded value.

--- app/src/main/java/com/example/ezvault/view/ItemsFragment.java
+++ app/src/main/java/com/example/ezvault/view/ItemsFragment.java
@@ -28,56 +28,11 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class ItemsFragment extends Fragment {
 
-    private FloatingActionButton floatbtn;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     @Inject
     public UserManager userManager;
 
-    // Item adapter for the recycler view
-    private ItemAdapter mItemAdapter;
-    private RecyclerView mRecyclerView;
-
     public ItemsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment items_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ItemsFragment newInstance(String param1, String param2) {
-        ItemsFragment fragment = new ItemsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
     }
 
     @Override
@@ -86,34 +41,32 @@ public class ItemsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_items, container, false);
 
         TextView filter = view.findViewById(R.id.text_filterSort);
-        filter.setOnClickListener(v -> {
-            Navigation.findNavController(view).navigate(R.id.itemsFragment_to_filterFragment);
-        });
+        filter.setOnClickListener(v ->
+            Navigation.findNavController(view).navigate(R.id.itemsFragment_to_filterFragment)
+        );
 
 
-        floatbtn = view.findViewById(R.id.button_add_item);
+        FloatingActionButton floatingButton = view.findViewById(R.id.button_add_item);
 
-        floatbtn.setOnClickListener(v -> {
-            Navigation.findNavController(view).navigate(R.id.itemsFragment_to_addItemFragment);
-        });
+        floatingButton.setOnClickListener(v ->
+            Navigation.findNavController(view).navigate(R.id.itemsFragment_to_addItemFragment)
+        );
 
-        mItemAdapter = new ItemAdapter(view.getContext(), userManager.getUser().getItemList(), new ItemAdapter.ItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-
-            }
-        });
+        // Item adapter for the recycler view
+        ItemAdapter itemAdapter = new ItemAdapter(view.getContext(),
+                userManager.getUser().getItemList(),
+                (v, position) -> {});
 
         // Setup recycler view
-        mRecyclerView = view.findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        mRecyclerView.setAdapter(mItemAdapter);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(itemAdapter);
 
-        mItemAdapter.setItems(userManager.getUser().getItemList());
+        itemAdapter.setItems(userManager.getUser().getItemList());
 
         // hide the empty items text if items exist
         TextView no_item_warning = view.findViewById(R.id.empty_items);
-        if (mItemAdapter.getItemCount() != 0) {
+        if (itemAdapter.getItemCount() != 0) {
             no_item_warning.setVisibility(View.GONE);
         }
         else {
@@ -122,18 +75,13 @@ public class ItemsFragment extends Fragment {
 
         // set the total quantity and total cost text
         TextView numItemsView = view.findViewById(R.id.text_number_of_items);
-        numItemsView.setText(String.valueOf(mItemAdapter.getItemCount()));
+        numItemsView.setText(String.valueOf(itemAdapter.getItemCount()));
 
         TextView totalItemValueView = view.findViewById(R.id.text_total_value);
-        //double totalItemValue = userManager.getUser().getItemList().getTotalValue();
-        //totalItemValueView.setText(String.valueOf(totalItemValue));
+        double totalItemValue = userManager.getUser().getItemList().getTotalValue();
+        totalItemValueView.setText(String.valueOf(totalItemValue));
 
         // Inflate the layout for this fragment
         return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 }
