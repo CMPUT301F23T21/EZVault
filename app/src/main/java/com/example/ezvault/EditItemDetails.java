cc91a9a39f49c072f4bf782e35aaf6c1eb9e0ea9
maodus <maodus@ualberta.ca>
2023-11-28 08:48:22 -0700

    Implemented the ability to add and remove images while editing items.
    Adding images and clicking save will create and add the stored images into the database. Oppositely, saving while having images removed will delete them from the database.

--- app/src/main/java/com/example/ezvault/EditItemDetails.java
+++ app/src/main/java/com/example/ezvault/EditItemDetails.java
@@ -1,41 +1,80 @@
 package com.example.ezvault;
 
 import android.app.DatePickerDialog;
+import android.content.ContentResolver;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.example.ezvault.camera.GalleryAction;
 import com.example.ezvault.database.FirebaseBundle;
+import com.example.ezvault.database.ImageDAO;
 import com.example.ezvault.database.ItemDAO;
+import com.example.ezvault.model.Image;
 import com.example.ezvault.model.Item;
-import com.google.android.material.datepicker.MaterialDatePicker;
+import com.example.ezvault.utils.FileUtils;
+import com.example.ezvault.utils.TaskUtils;
+import com.example.ezvault.utils.UserManager;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.android.material.timepicker.MaterialTimePicker;
-import com.google.android.material.timepicker.TimeFormat;
 import com.google.firebase.Timestamp;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
-import java.util.TimeZone;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+@AndroidEntryPoint
 public class EditItemDetails extends Fragment {
+
+    /**
+     * View model of item that is being currently edited
+     */
+    private MutableLiveData<Item> itemModel;
+
+    /**
+     * Adapter for slideshow recycler view
+     */
+    private PhotoAdapter photoAdapter;
+
+    /**
+     * Images to be shown in the recycler view slideshow
+     */
+    private ArrayList<Image> images;
+
+    private ContentResolver contentResolver;
+
+    @Inject
+    protected UserManager userManager;
+
+    /**
+     * Allows for the selection of photos from the photo gallery
+     */
+    private GalleryAction galleryAction;
+
     public EditItemDetails() {
         // Required empty public constructor
     }
@@ -43,14 +82,46 @@ public class EditItemDetails extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        itemModel = new ViewModelProvider(requireActivity()).get(ItemViewModel.class).get();
+
+        images = new ArrayList<>();
+        images.addAll(itemModel.getValue().getImages());
+
+        contentResolver = requireContext().getContentResolver();
+        photoAdapter = new PhotoAdapter(requireContext(), images, userManager);
+
+        galleryAction = new GalleryAction(requireActivity());
+        getLifecycle().addObserver(galleryAction);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        MutableLiveData<Item> item = new ViewModelProvider(requireActivity()).get(ItemViewModel.class).get();
         View view = inflater.inflate(R.layout.fragment_edit_item_details, container, false);
+
+        syncImages(contentResolver);
+
+        RecyclerView photoRecyclerView = view.findViewById(R.id.edit_details_image_recycler);
+        photoRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        photoRecyclerView.setAdapter(photoAdapter);
+
+        ImageButton takePhotoButton = view.findViewById(R.id.edit_take_pic);
+        ImageButton galleryButton = view.findViewById(R.id.edit_gallery);
+
+        takePhotoButton.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_editItemDetails_to_cameraFragment);
+        });
+
+        galleryButton.setOnClickListener(v -> {
+            galleryAction.resolveAll().continueWith(imTask -> {
+                userManager.getUriCache().addAll(imTask.getResult());
+                syncImages(contentResolver);
+                return null;
+            });
+        });
 
         Button saveButton = view.findViewById(R.id.edit_details_save);
         EditText makeText = view.findViewById(R.id.edit_details_make);
@@ -62,7 +133,7 @@ public class EditItemDetails extends Fragment {
         EditText serialText = view.findViewById(R.id.edit_details_serial_number);
         EditText dateText = view.findViewById(R.id.edit_details_date);
 
-        Item raw = item.getValue();
+        Item raw = itemModel.getValue();
         Log.v("EZVault", "Editing item: " + raw.getId());
 
         //set original details
@@ -90,7 +161,7 @@ public class EditItemDetails extends Fragment {
         dateLayout.setEndIconOnClickListener(v -> {
             Log.d("EZVault", "Setting new date.");
 
-            DatePickerDialog dialog = new DatePickerDialog(getContext());
+            DatePickerDialog dialog = new DatePickerDialog(requireContext());
             int y = calendar.get(Calendar.YEAR);
             int m = calendar.get(Calendar.MONTH);
             int d = calendar.get(Calendar.DAY_OF_MONTH);
@@ -126,13 +197,80 @@ public class EditItemDetails extends Fragment {
             raw.setSerialNumber(serial);
             raw.setAcquisitionDate(date);
 
-            new ItemDAO(new FirebaseBundle())
-                    .update(raw.getId(), raw)
-                    .addOnSuccessListener(x -> {
-                        saveButton.setEnabled(true);
-                    });
+            FirebaseBundle fb = new FirebaseBundle();
+            ImageDAO imageDAO = new ImageDAO(fb);
+
+            // Images that exist in the DB that the user removed
+            List<Image> removedImages = raw.getImages().stream()
+                    .filter(im -> !images.contains(im))
+                    .collect(Collectors.toList());
+
+            // Newly added images
+            List<Image> newImages = images.stream()
+                    .filter(im -> im.getId() == null)
+                    .collect(Collectors.toList());
+
+
+            List<Task<Void>> imageDeleteTasks = new ArrayList<>();
+            List<Task<String>> imageUploadTasks = new ArrayList<>();
+
+            if (!removedImages.isEmpty()){
+                removedImages.forEach(im -> imageDeleteTasks.add(imageDAO.delete(im.getId())));
+            }
+
+            if (!newImages.isEmpty()) {
+                newImages.forEach(im -> imageUploadTasks.add(imageDAO.create(im)));
+            }
+
+            Tasks.whenAllSuccess(imageDeleteTasks)
+                    .onSuccessTask(t -> Tasks.whenAllSuccess(imageUploadTasks)
+                            .onSuccessTask(uploadedImageTasks -> {
+                                for (int i = 0; i < newImages.size(); i++) {
+                                    newImages.get(i).setId((String) uploadedImageTasks.get(i));
+                                }
+
+                                raw.setImages((ArrayList<Image>) images.clone());
+
+                                new ItemDAO(fb)
+                                        .update(raw.getId(), raw)
+                                        .addOnSuccessListener(x -> {
+                                            userManager.clearUriCache();
+                                            saveButton.setEnabled(true);
+                                        });
+
+                                return TaskUtils.drop(Tasks.forResult(null));
+                            }));
+
+
         });
 
         return view;
+    }
+
+    /**
+     * Synchronizes the recycler view of Images with the state of the user's uri cache
+     * @param contentResolver Content resolver used for reading media
+     */
+    private void syncImages(ContentResolver contentResolver){
+        int nonDbImLen = (int) images.stream()
+                .filter(im -> im.getId() == null)
+                .count();
+
+        int imLen = images.size();
+        int lenDiff = imLen - nonDbImLen;
+        int cacheLen = userManager.getUriCache().size();
+
+        if (nonDbImLen < cacheLen) {
+            int numPhotosAdded = cacheLen - nonDbImLen;
+            int addedStart = cacheLen - numPhotosAdded;
+
+            for (int i = addedStart; i < cacheLen; i++){
+                Image image = FileUtils
+                        .imageFromUri(userManager.getUriCache().get(i), contentResolver);
+                images.add(image);
+                photoAdapter.notifyItemInserted(i + lenDiff);
+            }
+            photoAdapter.notifyItemRangeInserted(addedStart + lenDiff, cacheLen + lenDiff - 1);
+        }
     }
 }
