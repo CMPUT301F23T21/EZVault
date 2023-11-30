a1e8fd2b57b8c05509b91aef87dac6b91bb1bab7
maodus <maodus@ualberta.ca>
2023-11-29 18:44:56 -0700

    Added the property isSelected to an item to keep track of whether it is selected or not in the menu.

--- app/src/main/java/com/example/ezvault/model/Item.java
+++ app/src/main/java/com/example/ezvault/model/Item.java
@@ -69,6 +69,9 @@ public class Item {
      */
     private double count;
 
+    @Exclude
+    private boolean selected;
+
     /**
      * Create an item with a serial number.
      * @param id The id of the item
@@ -93,6 +96,7 @@ public class Item {
         this.images = images;
         this.count = count;
         this.value = value;
+        this.selected = false;
     }
 
     /**
@@ -265,5 +269,15 @@ public class Item {
             }
         }
         return false;
+    }
+
+    @Exclude
+    public void setSelected(boolean selected){
+        this.selected = selected;
+    }
+
+    @Exclude
+    public boolean isSelected(){
+        return this.selected;
     }
 }
