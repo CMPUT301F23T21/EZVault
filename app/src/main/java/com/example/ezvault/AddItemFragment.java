7341db52f7db53938c7e55b39b5ecd217f17911f
maodus <maodus@ualberta.ca>
2023-11-30 21:13:18 -0700

    Changed to Corbins UI for adding items. Also integrated the new cache system for images and improved database query performance for rawuser.

--- app/src/main/java/com/example/ezvault/AddItemFragment.java
+++ app/src/main/java/com/example/ezvault/AddItemFragment.java
@@ -2,12 +2,17 @@ package com.example.ezvault;
 
 import android.content.ContentResolver;
 import android.app.DatePickerDialog;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -39,6 +44,7 @@ import com.example.ezvault.utils.TaskUtils;
 import com.example.ezvault.utils.UserManager;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.Timestamp;
 
 import java.text.SimpleDateFormat;
@@ -53,6 +59,7 @@ import dagger.hilt.android.AndroidEntryPoint;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 /**
  * fragment class that collects the information of a new item
@@ -60,16 +67,7 @@ import java.util.Locale;
 @AndroidEntryPoint
 public class AddItemFragment extends Fragment {
 
-    private Button addItem;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button createButton;
 
     private GalleryAction galleryAction;
 
@@ -82,35 +80,22 @@ public class AddItemFragment extends Fragment {
 
     private ContentResolver contentResolver;
 
+    private boolean canInteract;
+
     public AddItemFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TagsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddItemFragment newInstance(String param1, String param2) {
-        AddItemFragment fragment = new AddItemFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        canInteract = true;
+
         contentResolver = requireContext().getContentResolver();
 
         images = new ArrayList<>();
-        photoAdapter = new PhotoAdapter(requireContext(), images, userManager);
+        photoAdapter = new PhotoAdapter(requireContext(), images);
 
         galleryAction = new GalleryAction(requireActivity());
         getLifecycle().addObserver(galleryAction);
@@ -128,7 +113,7 @@ public class AddItemFragment extends Fragment {
             }
             @Override
             public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
-                return false;
+                return !canInteract;
             }
         }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
@@ -137,27 +122,65 @@ public class AddItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_item, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_item_details, container, false);
 
-        syncImages(contentResolver);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return !canInteract;
+            }
+        });
 
-        RecyclerView photoRecyclerView = view.findViewById(R.id.add_item_recyclerview);
+        createButton = view.findViewById(R.id.edit_details_save);
+        createButton.setText("Create");
+
+        EditText itemMake = view.findViewById(R.id.edit_details_make);
+        EditText itemModel = view.findViewById(R.id.edit_details_model);
+        EditText itemDescription = view.findViewById(R.id.edit_details_description);
+        EditText itemComments = view.findViewById(R.id.edit_details_comment);
+        EditText itemQuantity = view.findViewById(R.id.edit_details_count);
+        EditText itemValue = view.findViewById(R.id.edit_details_value);
+        EditText itemSerial = view.findViewById(R.id.edit_details_serial_number);
+        EditText itemDate = view.findViewById(R.id.edit_details_date);
+
+        userManager.synchronizeToAdapter(images, photoAdapter);
+
+        RecyclerView photoRecyclerView = view.findViewById(R.id.edit_details_image_recycler);
         photoRecyclerView.setLayoutManager(new LinearLayoutManager(this.requireContext(), LinearLayoutManager.HORIZONTAL, false));
         photoRecyclerView.setAdapter(photoAdapter);
 
-        // Get all of our text fields
-        EditText itemName = view.findViewById(R.id.edittext_item_name);
-        EditText itemValue = view.findViewById(R.id.edittext_item_value);
-        EditText itemQuantity = view.findViewById(R.id.edittext_item_quantity);
-        EditText itemMake = view.findViewById(R.id.edittext_item_make);
-        EditText itemModel = view.findViewById(R.id.edittext_item_model);
-        EditText itemSerial = view.findViewById(R.id.edittext_item_serial);
-        EditText itemDescription = view.findViewById(R.id.edittext_item_description);
-        EditText itemComments = view.findViewById(R.id.edittext_item_comment);
-        EditText itemDate = view.findViewById(R.id.edittext_item_date);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String dateString = format.format(new Date());
 
-        addItem = view.findViewById(R.id.button_confirm_add_item);
-        addItem.setOnClickListener(v -> {
+        itemDate.setText(dateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        TextInputLayout dateLayout = view.findViewById(R.id.edit_details_date_layout);
+        dateLayout.setEndIconOnClickListener(v -> {
+            Log.d("EZVault", "Setting new date.");
+
+            DatePickerDialog dialog = new DatePickerDialog(requireContext());
+            int y = calendar.get(Calendar.YEAR);
+            int m = calendar.get(Calendar.MONTH);
+            int d = calendar.get(Calendar.DAY_OF_MONTH);
+            dialog.updateDate(y, m, d);
+            dialog.setOnDateSetListener((datePicker, year, month, day) -> {
+                calendar.set(year, month, day);
+
+                itemDate.setText(format.format(calendar.getTime()));
+            });
+            dialog.show();
+        });
+
+        createButton.setOnClickListener(v -> {
+            toggleInteractable();
+
             FirebaseBundle fb = new FirebaseBundle();
             ItemDAO itemDAO = new ItemDAO(fb);
             ImageDAO imageDAO = new ImageDAO(fb);
@@ -176,23 +199,13 @@ public class AddItemFragment extends Fragment {
                 return TaskUtils.drop(Tasks.forResult(null));
 
             }).continueWith(itemCreateTask -> {
-
-                String dateText = itemDate.getText().toString();
-                Date realItemDate;
-
-                if (dateText.isEmpty()){
-                    realItemDate = new Date();
-                } else {
-                    realItemDate = new SimpleDateFormat("dd-MM-yyyy").parse(dateText);
-                }
-
                 // Construct our item
                 ItemBuilder itemBuilder = new ItemBuilder()
                         .setMake(itemMake.getText().toString())
                         .setModel(itemModel.getText().toString())
                         .setCount(Double.valueOf(itemQuantity.getText().toString()))
                         .setValue(Double.valueOf(itemValue.getText().toString()))
-                        .setAcquisitionDate(new Timestamp(realItemDate))
+                        .setAcquisitionDate(new Timestamp(calendar.getTime()))
                         .setDescription(itemDescription.getText().toString())
                         .setSerialNumber(itemSerial.getText().toString())
                         .setComment(itemComments.getText().toString())
@@ -213,22 +226,30 @@ public class AddItemFragment extends Fragment {
 
                     RawUserDAO rawUserDAO = new RawUserDAO(fb);
 
-                    // Fetch our raw local user
-                    // TODO: Expose raw user so we dont have to fetch every tag/item creation or update
-                    rawUserDAO.read(uid).continueWith(rawUserTask -> {
-                        RawUserDAO.RawUser rawUser = rawUserTask.getResult();
-                        rawUser.getItemids().add(itemId); // Add the item to our user database reference
+                    List<String> itemIds = localUser.getItemList()
+                            .stream()
+                            .map(item -> item.getId())
+                            .collect(Collectors.toList());
 
-                        rawUserDAO.update(uid, rawUser)
-                                .continueWith(updateUserTask -> {
-                                    images.clear();
-                                    userManager.clearUriCache();
-                                    Navigation.findNavController(view).popBackStack();
-                                    return null;
-                                });
+                    List<String> tagIds = localUser
+                            .getItemList()
+                            .getTags()
+                            .stream()
+                            .map(tag -> tag.getUid())
+                            .collect(Collectors.toList());
 
-                        return null;
-                    });
+                    // Update raw user
+                    RawUserDAO.RawUser rawUser = new RawUserDAO.RawUser(localUser.getUserName(),
+                            (ArrayList<String>) tagIds,
+                            (ArrayList<String>) itemIds);
+
+                    rawUserDAO.update(uid, rawUser)
+                            .continueWith(updateUserTask -> {
+                                images.clear();
+                                userManager.clearLocalImages();
+                                Navigation.findNavController(view).popBackStack();
+                                return null;
+                            });
 
                     return null;
                 });
@@ -237,60 +258,37 @@ public class AddItemFragment extends Fragment {
         });
 
         // Handle opening the camera
-        ImageButton cameraButton = view.findViewById(R.id.imageButton);
+        ImageButton cameraButton = view.findViewById(R.id.edit_take_pic);
         cameraButton.setOnClickListener(v -> {
             Navigation.findNavController(view).navigate(R.id.action_addItemFragment_to_cameraFragment);
         });
 
         // Handle opening the gallery
-        ImageButton galleryButton = view.findViewById(R.id.imageButton2);
+        ImageButton galleryButton = view.findViewById(R.id.edit_gallery);
         galleryButton.setOnClickListener(v -> {
             galleryAction.resolveAll().continueWith(imTask -> {
-                userManager.getUriCache().addAll(imTask.getResult());
-                syncImages(contentResolver);
+                List<Uri> uris = imTask.getResult();
+
+                if (uris != null && uris.size() > 0){
+                    uris.forEach(uri -> {
+                        userManager.addLocalImage(FileUtils.imageFromUri(uri, contentResolver));
+                    });
+
+                    userManager.synchronizeToAdapter(images, photoAdapter);
+                }
+
                 return null;
             });
         });
 
-        SimpleDateFormat format = new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault());
-        Calendar calendar = Calendar.getInstance();
-        calendar.clear();
-
-        itemDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext());
-                datePickerDialog.setOnDateSetListener((DatePicker, year, month, day) -> {
-                    calendar.set(year, month, day);
-
-                    itemDate.setText(format.format(calendar.getTime()));
-                });
-                datePickerDialog.show();
-            }
-        });
         return view;
     }
 
-    /**
-     * Synchronizes the recycler view of Images with the state of the user's uri cache
-     * @param contentResolver Content resolver used for reading media
-     */
-    private void syncImages(ContentResolver contentResolver){
-        int imLen = images.size();
-        int cacheLen = userManager.getUriCache().size();
+    private void toggleInteractable(){
+        canInteract = !canInteract;
 
-        if (imLen == cacheLen) return;
-        else if (imLen < cacheLen) {
-            int numPhotosAdded = cacheLen - imLen;
-            int addedStart = cacheLen - numPhotosAdded;
-
-            for (int i = addedStart; i < cacheLen; i++){
-                Image image = FileUtils
-                        .imageFromUri(userManager.getUriCache().get(i), contentResolver);
-                images.add(image);
-                photoAdapter.notifyItemInserted(i);
-            }
-            photoAdapter.notifyItemRangeInserted(addedStart, cacheLen - 1);
-        }
+        createButton.setEnabled(canInteract);
+        createButton.setClickable(canInteract);
+        createButton.getBackground().setAlpha(canInteract ? 255 : 112);
     }
 }
