b879e3fc19a35f0c78452f318f32f675b090b53e
maodus <maodus@ualberta.ca>
2023-12-02 00:35:20 -0700

    Implemented the ability to change: username, email, and password.

--- app/src/main/java/com/example/ezvault/ProfileFragment.java
+++ app/src/main/java/com/example/ezvault/ProfileFragment.java
@@ -6,14 +6,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.os.UserManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ezvault.database.FirebaseBundle;
+import com.example.ezvault.database.RawUserDAO;
+import com.example.ezvault.model.ItemList;
+import com.example.ezvault.utils.UserManager;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -22,7 +31,13 @@ import dagger.hilt.android.AndroidEntryPoint;
 /**
  * Displays the user profile and handles changes in username, email or password
  */
+@AndroidEntryPoint
 public class ProfileFragment extends Fragment {
+
+    @Inject
+    protected UserManager userManager;
+
+    private FirebaseBundle firebaseBundle;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -31,6 +46,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseBundle = new FirebaseBundle();
     }
 
     @Override
@@ -39,13 +55,77 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        String currentUsername = userManager.getUser()
+                .getUserName();
+
+        String currentEmail = firebaseBundle.getAuth()
+                .getCurrentUser()
+                .getEmail();
+
+        EditText usernameText = view.findViewById(R.id.edittext_profile_username);
+        usernameText.setText(currentUsername);
+
+        EditText emailText = view.findViewById(R.id.edittext_profile_email);
+        emailText.setText(currentEmail);
+
+        EditText newPasswordText = view.findViewById(R.id.edittext_profile_password);
+
         Button logOutButton = view.findViewById(R.id.log_out_button);
-
         logOutButton.setOnClickListener(v -> {
-            FirebaseBundle currFb = new FirebaseBundle();
-            currFb.getAuth().signOut();
-
+            firebaseBundle.getAuth().signOut();
             Navigation.findNavController(view).navigate(R.id.profileFragment_to_welcomeFragment);
+        });
+
+        Button updateButton = view.findViewById(R.id.update_info_button);
+        updateButton.setOnClickListener(v -> {
+            List<Task<Void>> profileTasks = new ArrayList<>();
+
+            String updatedUsername = usernameText.getText().toString();
+            if (!updatedUsername.equals(currentUsername)){
+                ItemList itemsList = userManager.getUser().getItemList();
+
+                RawUserDAO.RawUser newRawUser = new RawUserDAO.RawUser(updatedUsername,
+                        (ArrayList<String>)itemsList.getTagIds(),
+                        (ArrayList<String>)itemsList.getItemIds());
+
+                Task<Void> updateUsernameTask = new RawUserDAO(firebaseBundle)
+                        .update(userManager.getUser().getUid(), newRawUser);
+
+                updateUsernameTask.addOnSuccessListener(t -> {
+                    userManager.getUser()
+                            .applyUserNameUpdate(updatedUsername);
+                });
+
+                profileTasks.add(updateUsernameTask);
+            }
+
+            String updatedEmail = emailText.getText().toString();
+            if (!updatedEmail.equals(currentEmail)){
+                profileTasks.add(firebaseBundle.getAuth().getCurrentUser().updateEmail(updatedEmail));
+            }
+
+            String updatedPassword = newPasswordText.getText().toString();
+            if (!updatedPassword.isEmpty()){
+                profileTasks.add(firebaseBundle.getAuth().getCurrentUser().updatePassword(updatedPassword));
+            }
+
+            if (profileTasks.isEmpty()) return; // We didn't do anything
+
+            Tasks.whenAllComplete(profileTasks)
+                    .addOnSuccessListener(t -> {
+                        Toast.makeText(requireContext(),
+                                "Profile sucessfully updated",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+
+                        newPasswordText.setText("");
+                     }).addOnFailureListener(e -> {
+                        Toast.makeText(requireContext(),
+                                "Error updating profile, please try again",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                     });
+
         });
 
         return view;
