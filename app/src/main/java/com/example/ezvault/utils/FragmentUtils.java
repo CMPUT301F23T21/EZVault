6cd92be452bcdcd369ec873b2157c9713e10e7f1
maodus <maodus@ualberta.ca>
2023-12-02 07:21:37 -0700

    Added new utility helper for fragments.

---
+++ app/src/main/java/com/example/ezvault/utils/FragmentUtils.java
@@ -0,0 +1,30 @@
+package com.example.ezvault.utils;
+
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class FragmentUtils {
+
+    public static TextInputLayout getTextParentLayout(EditText textField){
+        return (TextInputLayout)((ViewGroup)textField.getParent()).getParent();
+    }
+
+    public static boolean textHasNoErrors(TextView... args){
+        return Arrays.stream(args)
+                .map(TextView::getError)
+                .allMatch(Objects::isNull);
+    }
+
+    public static boolean textLayoutHasNoErrors(TextInputLayout... args){
+        return Arrays.stream(args)
+                .map(TextInputLayout::getError)
+                .allMatch(Objects::isNull);
+    }
+}
