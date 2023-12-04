226b4c75a8d216e2cedf4018bede9db7c833c970
nortif8 <nortif8@gmail.com>
2023-12-04 02:12:07 -0700

    refactor(package): move viewmodels into viewmodel

--- app/src/main/java/com/example/ezvault/viewmodel/SelectedItemViewModel.java
+++ app/src/main/java/com/example/ezvault/viewmodel/SelectedItemViewModel.java
@@ -1,11 +1,11 @@
-package com.example.ezvault;
+package com.example.ezvault.viewmodel;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.ezvault.model.Item;
 
-public class ItemViewModel extends ViewModel {
+public class SelectedItemViewModel extends ViewModel {
     private final MutableLiveData<Item> itemData = new MutableLiveData<>();
 
     public void set(Item item) {
